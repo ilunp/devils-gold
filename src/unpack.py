532e2aeec73b4e34458d3e18ebf8d3dd59745578
Clem Freeman <10442838+clemf@users.noreply.github.com>
2024-11-18 12:34:11 -0800

    Unpack all from loot table

--- src/unpack.py
+++ src/unpack.py
@@ -4,15 +4,17 @@ import argparse
 import json
 import re
 
+UNPACK_DIR = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', 'unpacked'))
+if not os.path.exists(UNPACK_DIR):
+    os.makedirs(UNPACK_DIR)
+
+DEFAULT_PATH = r'C:\Program Files (x86)\Steam\steamapps\common\SULFUR\Sulfur_Data\StreamingAssets\aa\StandaloneWindows64'
+
 parser = argparse.ArgumentParser("unpack")
 parser.add_argument(
-    "assetdir", help="Directory where sulfur assets are located", type=str)
+    "--assetpath", help="Asset directory or file to unpack", type=str, default=DEFAULT_PATH, required=False)
 args = parser.parse_args()
-
-unpack_dir = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..', 'unpacked'))
-if not os.path.exists(unpack_dir):
-    os.makedirs(unpack_dir)
 
 
 def clean_file_name(name: str):
@@ -43,7 +45,21 @@ def unpack_file(root, file_name, destination_folder):
         except:
             print('error')
         if assetType == "MonoBehaviour":
+            breakpoint()
             write_asset(pptr, destination_folder)
+
+
+def unpack_loot_table(source):
+    env = UnityPy.load(source)
+    for pptr in env.objects:
+        if pptr.path_id == -2273047535729816587:
+            loot_table = pptr.parse_as_object()
+            name = loot_table.m_Name
+            unpack_dir = f'{UNPACK_DIR}/{name}'
+            if not os.path.exists(unpack_dir):
+                os.makedirs(unpack_dir)
+            for loot in loot_table.entries:
+                write_asset(loot.lootItem, unpack_dir)
 
 
 def write_asset(pptr, destination_folder):
@@ -58,4 +74,5 @@ def write_asset(pptr, destination_folder):
         json.dump(tree, f, ensure_ascii=False, indent=4)
 
 
-unpack_all_assets(args.assetdir, unpack_dir)
+# unpack_all_assets(args.assetpath, UNPACK_DIR)
+unpack_loot_table(args.assetpath)
