4f5de75e25a08ffeeaf7b5829fa9218293a10149
Clem Freeman <10442838+clemf@users.noreply.github.com>
2024-11-18 11:25:16 -0800

    Working extract all monobehaviours

--- src/unpack.py
+++ src/unpack.py
@@ -2,10 +2,11 @@ import os
 import UnityPy
 import argparse
 import json
+import re
 
 parser = argparse.ArgumentParser("unpack")
-parser.add_argument("assetdir",
-                    help="Directory where sulfur assets are located", type=str)
+parser.add_argument(
+    "assetdir", help="Directory where sulfur assets are located", type=str)
 args = parser.parse_args()
 
 unpack_dir = os.path.abspath(os.path.join(
@@ -14,28 +15,47 @@ if not os.path.exists(unpack_dir):
     os.makedirs(unpack_dir)
 
 
-def unpack_all_assets(source_folder: str, destination_folder: str):
-    # iterate over all files in source folder
-    for root, dirs, files in os.walk(source_folder):
+def clean_file_name(name: str):
+    # Removes invalid chars from a filename str
+    return re.sub(r"[/\\?%*:|\"<>\x7F\x00-\x1F]", "-", name)
+
+
+def unpack_all_assets(source: str, destination_folder: str):
+    if os.path.isfile(source):
+        root = os.path.dirname(source)
+        file_name = os.path.basename(source)
+        unpack_file(root, file_name, destination_folder)
+    for root, dirs, files in os.walk(source):
         for file_name in files:
-            print('PARSING FILE: ' + file_name)
-            # generate file_path
-            file_path = os.path.join(root, file_name)
-            # load that file via UnityPy.load
-            env = UnityPy.load(file_path)
-            for path, pptr in env.container.items():
-                isType = False
-                try:
-                    isType = pptr.type.name == "MonoBehaviour"
-                except:
-                    print('error: ' + path)
-                if isType:
-                    data = pptr.deref()
-                    tree = data.read_typetree()
-                    fp = os.path.join(destination_folder, f"{
-                                      tree['m_Name']}.json")
-                    with open(fp, "wt", encoding="utf8") as f:
-                        json.dump(tree, f, ensure_ascii=False, indent=4)
+            unpack_file(root, file_name, destination_folder)
+
+
+def unpack_file(root, file_name, destination_folder):
+    # generate file_path
+    file_path = os.path.join(root, file_name)
+    print('PARSING FILE: ' + os.path.join(file_path))
+    # load that file via UnityPy.load
+    env = UnityPy.load(file_path)
+    for pptr in env.objects:
+        assetType = ''
+        try:
+            assetType = pptr.type.name
+        except:
+            print('error')
+        if assetType == "MonoBehaviour":
+            write_asset(pptr, destination_folder)
+
+
+def write_asset(pptr, destination_folder):
+    tree = pptr.read_typetree()
+    name = ''
+    if 'm_Name' in tree:
+        name = tree['m_Name']
+    else:
+        name = pptr.path_id
+    fp = os.path.join(destination_folder, f"{clean_file_name(name)}.json")
+    with open(fp, "wt", encoding="utf8") as f:
+        json.dump(tree, f, ensure_ascii=False, indent=4)
 
 
 unpack_all_assets(args.assetdir, unpack_dir)
